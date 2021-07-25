2b8a5bc19ca9019d8b7d8a7d78631f021ed427dc
Andre Rodrigues Melgaço dos Santos <dev-asantos@outlook.com>
2021-07-25 14:48:42 -0400

    Estrutura para iniciar projeto - finalizada

--- src/App.test.js
+++ src/App.test.js
@@ -1,8 +1,8 @@
-import { render, screen } from '@testing-library/react';
+/* eslint-disable no-undef */
+import { render } from '@testing-library/react';
+
 import App from './App';
 
-test('renders learn react link', () => {
+test('Deve funcionar sem problemas', () => {
   render(<App />);
-  const linkElement = screen.getByText(/learn react/i);
-  expect(linkElement).toBeInTheDocument();
 });
