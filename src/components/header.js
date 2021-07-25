f22872e66e5e45aae6e3a9f16a83c1f8bda74a15
Andre Rodrigues Melgaço dos Santos <dev-asantos@outlook.com>
2021-07-25 15:48:15 -0400

    Uso de Header

--- src/components/header.js
+++ src/components/header.js
@@ -1,3 +1,7 @@
-const Header = () => <h1 className='title-principal'>Gereciador de Notas</h1>;
+const Header = () => (
+  <header>
+    <h1 className='title-principal'>Gereciador de Notas</h1>
+  </header>
+);
 
 export default Header;
