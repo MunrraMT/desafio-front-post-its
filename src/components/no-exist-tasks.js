af659ddb14e5225d29872a9ed06853f10221f573
Andre Rodrigues Melgaço dos Santos <dev-asantos@outlook.com>
2021-07-25 16:34:46 -0400

    Abstração para o componente NoExistTasks

---
+++ src/components/no-exist-tasks.js
@@ -0,0 +1,10 @@
+import BtnAddNotas from './btn-add-notas';
+
+const NoExistTasks = () => (
+  <main className='ver-notas__content'>
+    <h3 className='ver-notas__title'>Não existem notas adicionadas</h3>
+    <BtnAddNotas />
+  </main>
+);
+
+export default NoExistTasks;
