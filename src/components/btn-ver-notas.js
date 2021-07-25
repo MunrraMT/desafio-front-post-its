8cf8adbf3f043ea29746e75d316dc874410121e0
Andre Rodrigues Melgaço dos Santos <dev-asantos@outlook.com>
2021-07-25 16:13:10 -0400

    add componente BtnVerNotas

---
+++ src/components/btn-ver-notas.js
@@ -0,0 +1,17 @@
+import { useHistory } from 'react-router-dom';
+
+const BtnVerNotas = () => {
+  const history = useHistory();
+
+  const handleClick = () => {
+    history.push('/ver-notas');
+  };
+
+  return (
+    <button onClick={handleClick} className='home__btn' type='button'>
+      ver notas
+    </button>
+  );
+};
+
+export default BtnVerNotas;
