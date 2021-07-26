5187ef02b03992cefdb0969873a690f7f1e7d380
Andre Rodrigues Melgaço dos Santos <dev-asantos@outlook.com>
2021-07-26 00:42:19 -0400

    Add classes

--- src/components/show-tasks.js
+++ src/components/show-tasks.js
@@ -1,20 +1,27 @@
+import BtnAddNotas from './btn-add-notas';
+
 const ShowTasks = (tasks) => {
   const tasksFormated = tasks.map((task) => (
-    <article key={task.id}>
-      <header>
+    <article className='task__content' key={task.id}>
+      <header className='task__header'>
         <h3>{task.title}</h3>
       </header>
-      <p>{task.text}</p>
-      <footer>
-        <button type='button'>x</button>
+      <p className='task__text'>{task.text}</p>
+      <footer className='task__footer'>
+        <button className='task__btn-delete' type='button'>
+          <i className='fas fa-times' />
+        </button>
       </footer>
     </article>
   ));
 
   return (
-    <main>
-      <h2>Suas notas</h2>
-      <section>{tasksFormated}</section>
+    <main className='show-tasks__content'>
+      <h2 className='show-tasks__h2'>Suas notas</h2>
+
+      <section className='show-tasks__tasks'>{tasksFormated}</section>
+
+      <BtnAddNotas />
     </main>
   );
 };
